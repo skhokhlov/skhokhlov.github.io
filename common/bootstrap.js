8c5b2fb7fb5d9c6ef0d537d9fc9ced6ec0600b3e
skhokhlov <worldfly@yandex.ru>
2015-05-12 21:15:08 +0300

    Clean up bootstrap.js

--- common/bootstrap.js
+++ common/bootstrap.js
@@ -1,33 +1,17 @@
 (function () {
-
-    /**
-     * XHR GET async request to url with Promises
-     * @param url Request address
-     * @returns {Promise} Response, Error
-     */
-    var request = function (url) {
-        return new Promise(function (resolve, reject) {
-            var Request = new XMLHttpRequest();
-            Request.open('GET', url, true);
-            Request.onload = function () {
-                if (Request.status === 200) {
-                    resolve(Request.response);
-                } else {
-                    reject(Error(Request.statusText));
-                }
-            };
-            Request.onerror = function () {
-                reject(Error("Network Error"));
-            };
-            Request.send(null);
-        });
+    var Request = new XMLHttpRequest();
+    Request.open('GET', url, true);
+    Request.onload = function (res) {
+        if (Request.status === 200) {
+            document.getElementsByClassName('landings-wrapper')[0].innerHTML = res;
+            document.getElementsByClassName('landings-link')[0].setAttribute('class', 'landings-link landings-link_active');
+            document.getElementsByClassName('landings-wrapper')[0].setAttribute('class', 'landings-wrapper landings-wrapper_active');
+        } else {
+            Error(Request.statusText);
+        }
     };
-
-    request('/common/landings.html').then(function (res) {
-        document.getElementsByClassName('landings-wrapper')[0].innerHTML = res;
-        document.getElementsByClassName('landings-link')[0].setAttribute('class', 'landings-link landings-link_active');
-        document.getElementsByClassName('landings-wrapper')[0].setAttribute('class', 'landings-wrapper landings-wrapper_active');
-    });
-
-
+    Request.onerror = function () {
+        reject(Error("Network Error"));
+    };
+    Request.send(null);
 })();
