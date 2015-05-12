ca6db12120fee78f9397e864bd0b29e0bb9fe594
skhokhlov <worldfly@yandex.ru>
2015-05-12 21:19:13 +0300

    Bug fix

--- common/bootstrap.js
+++ common/bootstrap.js
@@ -1,9 +1,9 @@
 (function () {
     var Request = new XMLHttpRequest();
     Request.open('GET', '/common/landings.html', true);
-    Request.onload = function (res) {
+    Request.onload = function () {
         if (Request.status === 200) {
-            document.getElementsByClassName('landings-wrapper')[0].innerHTML = res;
+            document.getElementsByClassName('landings-wrapper')[0].innerHTML = Request.responseText;
             document.getElementsByClassName('landings-link')[0].setAttribute('class', 'landings-link landings-link_active');
             document.getElementsByClassName('landings-wrapper')[0].setAttribute('class', 'landings-wrapper landings-wrapper_active');
         } else {
