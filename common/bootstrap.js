db88cef0d458a6c696f34c494d5ea1af070bcd2a
Sergey Khokhlov <sergey.a.khokhlov@gmail.com>
2015-06-19 10:59:37 +0300

    Fix bug
    Deleting undefined function

--- common/bootstrap.js
+++ common/bootstrap.js
@@ -7,11 +7,11 @@
             document.getElementsByClassName('landings-link')[0].setAttribute('class', 'landings-link landings-link_active');
             document.getElementsByClassName('landings-wrapper')[0].setAttribute('class', 'landings-wrapper landings-wrapper_active');
         } else {
-            Error(Request.statusText);
+            throw new Error(Request.statusText);
         }
     };
     Request.onerror = function () {
-        reject(Error("Network Error"));
+        throw new Error("Network Error"));
     };
     Request.send(null);
 })();
