ae9bfb0a07702faabd94dda26de47958bad6a0ca
skhokhlov <worldfly@yandex.ru>
2015-06-25 10:55:39 +0300

    Performance improvements

--- common/bootstrap.js
+++ common/bootstrap.js
@@ -1,17 +1,17 @@
 (function () {
-    var Request = new XMLHttpRequest();
-    Request.open('GET', '/common/landings.html', true);
-    Request.onload = function () {
-        if (Request.status === 200) {
-            document.getElementsByClassName('landings-wrapper')[0].innerHTML = Request.responseText;
+    var request = new XMLHttpRequest();
+    request.open('GET', '/common/landings.html', true);
+    request.onload = function () {
+        if (request.status === 200) {
+            document.getElementsByClassName('landings-wrapper')[0].innerHTML = request.responseText;
             document.getElementsByClassName('landings-link')[0].setAttribute('class', 'landings-link landings-link_active');
             document.getElementsByClassName('landings-wrapper')[0].setAttribute('class', 'landings-wrapper landings-wrapper_active');
         } else {
-            throw new Error(Request.statusText);
+            throw new Error(request.statusText);
         }
     };
-    Request.onerror = function () {
+    request.onerror = function () {
         throw new Error("Network Error");
     };
-    Request.send(null);
+    request.send(null);
 })();
